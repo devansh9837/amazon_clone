6d3b67a732a776eeeed42c22a1ef09caacfb2bd6
devansh9837 <devanshagarwal2020@gmail.com>
2020-11-12 03:58:16 +0530

    Updates

---
+++ precache-manifest.74ddc4d235a13bbf62b178f4dcde422a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8b5ff2ce4892cdfedbbc2d05193841b",
+    "url": "/devansh9837.github.io/amazon_clone/index.html"
+  },
+  {
+    "revision": "19d3385b85f11cb12d72",
+    "url": "/devansh9837.github.io/amazon_clone/static/css/main.d30b8548.chunk.css"
+  },
+  {
+    "revision": "cc53ade8f029deec476d",
+    "url": "/devansh9837.github.io/amazon_clone/static/js/2.0760f56e.chunk.js"
+  },
+  {
+    "revision": "890fe60e51f18d9e8e0004117fd20dc3",
+    "url": "/devansh9837.github.io/amazon_clone/static/js/2.0760f56e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19d3385b85f11cb12d72",
+    "url": "/devansh9837.github.io/amazon_clone/static/js/main.58d0b563.chunk.js"
+  },
+  {
+    "revision": "68b2a9f7621b5f976ad8",
+    "url": "/devansh9837.github.io/amazon_clone/static/js/runtime-main.f830b75d.js"
+  }
+]);
